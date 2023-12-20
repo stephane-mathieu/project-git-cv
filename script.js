8fd7ac6d795e336467214cce78bd25ab67b30f74
stephane-mathieu <stephane.mathieu111@gmail.com>
2023-12-20 02:39:38 -0500

    ajout script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showCV(cvNumber) {
+    // Charger le contenu du CV en fonction du numéro
+    const cvContainer = document.getElementById("cv-container");
+
+    // Vous pouvez remplacer le contenu des CVs par vos propres données
+    const cvContent = `<h2>CV ${cvNumber}</h2>
+                       <img src="lien_de_votre_image" alt="Photo du CV ${cvNumber}">
+                       <p>Informations sur le CV ${cvNumber}...</p>`;
+
+    cvContainer.innerHTML = cvContent;
+}
